6f9381a12a6701e3ed0e6157f72f5fdae3953ffb
Dr Nic Williams <drnicwilliams@gmail.com>
2010-07-11 17:54:03 +1000

    'gem' snippet asks for gem name and inserts largest local gem version

---
+++ Support/bin/gem_command.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+$:.unshift(File.dirname(__FILE__) + "/../lib")
+$:.push(File.join(ENV['TM_SUPPORT_PATH'], 'lib')) if ENV['TM_SUPPORT_PATH']
+
+require "ui"
+
+default_string = ENV['TM_CURRENT_WORD'].strip
+default_string = `pbpaste`.strip if default_string.size == 0
+
+if gem_name = TextMate::UI.request_string(:title => 'Enter gem name:', :default => default_string)
+  require "rubygems"
+  gem_specs = Gem.source_index.select do |gem| 
+    gem.first =~ /^#{gem_name}-\d/
+  end
+  if gem_spec = gem_specs.last
+    print "gem '#{gem_name}', '#{gem_spec.last.version}'"
+  else
+    print "gem '#{gem_name}'"
+  end
+else
+  print "gem"
+end
